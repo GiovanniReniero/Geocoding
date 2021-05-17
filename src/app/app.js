9012f5d883c3aaa754297639654fc620700b245c
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-05-17 10:50:18 +0200

    Added webpack.config.js file to create a new bundle file

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,7 @@ import {hitSubmit} from "./utilities/hitSubmit.js"
 console.log ("Vai Giovanni!!! Fino a qui ci arrivi")
 //API key = pk.eyJ1IjoiZ2lvcmVuIiwiYSI6ImNrb3F4b2piMjB6djIyeW51MXRrNDlibnAifQ.Xrh4UH-0RwRGCRPRxl-EpA
 
-  const userEntry = document.querySelector("form")
-  userEntry.addEventListener("submit", hitSubmit)
+const userEntry = document.querySelector("form")
+userEntry.addEventListener("submit", hitSubmit)
 
+  
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-console.log("Whelahhh, ghe sem!!!!")
+console.log("Whelahhh, vai GIovanni!!!!")
 
 import {hitSubmit} from "./app/utilities/hitSubmit.js"
 
 const userEntry = document.querySelector("form")
 userEntry.addEventListener("submit", hitSubmit)
+
