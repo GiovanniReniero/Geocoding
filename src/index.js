f4a94c63eb7e9e628ebf4a0a9355eb4242846813
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-05-17 15:28:07 +0200

    Added cash busting and HTMLWebpack Plugin with template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {hitSubmit} from "./app/utilities/hitSubmit.js"
 import "./style.css"
 
-console.log("Whelahhh, Nicaragua.. vai Giovanni!!!!")
+console.log("Whelahhh, Nicaragua, Paraguai, Uruguai... vai Giovai, vai!!!!")
 
 const userEntry = document.querySelector("form")
 userEntry.addEventListener("submit", hitSubmit)
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require ("path")
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { template } = require("lodash");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    }),
+  ],
   module: {
     rules: [
       {
