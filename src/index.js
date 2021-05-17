561085af45593feb5f085bfbfff66fe3076f7ad5
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-05-17 14:44:10 +0200

    added CSS loaders and css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-console.log("Whelahhh, vai GIovanni!!!!")
-
 import {hitSubmit} from "./app/utilities/hitSubmit.js"
+import "./style.css"
+
+console.log("Whelahhh, Nicaragua.. vai Giovanni!!!!")
 
 const userEntry = document.querySelector("form")
 userEntry.addEventListener("submit", hitSubmit)
