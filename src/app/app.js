d663206898011e80f829243349907047bfb6bb48
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-05-18 22:41:24 +0200

    Added default location of ROme on start up of application

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,12 @@
  // import _ from 'loadash'
 // import axios from 'axios'
-import {hitSubmit} from "./utilities/hitSubmit.js"
+// import {hitSubmit} from "./utilities/hitSubmit.js"
 
 
-console.log ("Vai Giovanni!!! Fino a qui ci arrivi")
+// console.log ("Vai Giovanni!!! Fino a qui ci arrivi")
 //API key = pk.eyJ1IjoiZ2lvcmVuIiwiYSI6ImNrb3F4b2piMjB6djIyeW51MXRrNDlibnAifQ.Xrh4UH-0RwRGCRPRxl-EpA
 
-const userEntry = document.querySelector("form")
-userEntry.addEventListener("submit", hitSubmit)
+  // const userEntry = document.querySelector("form")
+  // userEntry.addEventListener("submit", hitSubmit)
 
   
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {hitSubmit} from "./app/utilities/hitSubmit.js"
+import {urlBuilder} from "./app/utilities/urlBuilder.js"
 import "./style.css"
 
 console.log("Whelahhh, Nicaragua, Paraguai, Uruguai... vai Giovai, vai!!!!")
@@ -6,3 +7,9 @@ console.log("Whelahhh, Nicaragua, Paraguai, Uruguai... vai Giovai, vai!!!!")
 const userEntry = document.querySelector("form")
 userEntry.addEventListener("submit", hitSubmit)
 
+urlBuilder("Roma")
+
+// 1) Needs some CSS styling
+// 2) Nice to have would be to choose which place to see on map
+// 3) Implement places of interest around chosen map
+// 4) Offer different styles of map to the end user (satelite and 3d buildings)
